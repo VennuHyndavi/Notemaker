dc8f5ccf7db419a3594b8a4c048ecb39148a0ec6
VennuHyndavi <43744805+VennuHyndavi@users.noreply.github.com>
2024-08-23 01:09:33 +0530

    Add files via upload

---
+++ api/backend/notemaker/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Note(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
---
+++ api/backend/notemaker/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import NoteSerializer
+from .models import Note
+
+
+# Create your views here.
+
+
+class NoteView(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
+
